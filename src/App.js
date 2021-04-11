08759f325884512acfc482065bc88400e79aa850
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-04-11 14:57:09 +0100

    add delete function

--- src/App.js
+++ src/App.js
@@ -27,11 +27,15 @@ const App = () => {
     ] 
 )
 
+//delete Task
+const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+}
 
   return (
     <div className="container">
      <Header />
-     <Tasks tasks={tasks}/>
+     <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   )
 }
