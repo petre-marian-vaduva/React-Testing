40e02f0155f417a58a09a49df79930e80f347d3b
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-04-12 20:53:24 +0100

    add toggle function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddTask from './components/AddTask'
 
 const App = () => {
 
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState(
     [
         {
@@ -31,6 +32,7 @@ const App = () => {
 //Add task
 const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) +1
+    const newTask = [id, ...task]
     setTasks([...tasks, newTask])
 }
 
@@ -49,8 +51,9 @@ const toggleReminder = (id) => {
 
   return (
     <div className='container'>
-     <Header />
-     <AddTask onAdd={addTask}/>
+     <Header onAdd={() => setShowAddTask(!showAddTask)} 
+     showAdd={showAddTask}/>
+     {showAddTask &&  <AddTask onAdd={addTask}/>}
      {tasks.length > 0 ? (
      <Tasks tasks={tasks} onDelete=
      {deleteTask} onToggle={toggleReminder} /> 
