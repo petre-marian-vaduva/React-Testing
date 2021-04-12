5c342d59d9d68fbc8e687c47cf44f375ce994990
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-04-12 10:39:17 +0100

    Update AddTask.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import {useState} from 'react'
 
 const AddTask = () => {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState('false')
+
     return (
         <form className='add-form'>
             <div className='form-control'>
                 <label>Task</label>
-                <input type='text' placeholder='Add task' />
+                <input type='text' 
+                placeholder='Add task'
+                value={text} 
+                onChange={(e) => setText(e.target.value)}/>
             </div>
             <div className='form-control'>
                 <label>Day & Time</label>
-                <input type='text' placeholder='Add day & time' />
+                <input type='text' placeholder='Add day & time' value={day} 
+                onChange={(e) => setDay(e.target.value)}/>
             </div>
             <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
-                <input type='checkbox' />
+                <input type='checkbox' value={reminder} 
+                onChange={(e) => setReminder(e.currentTarget.checked)}/>
             </div>
             <input type='submit' value='Save Task' className='btn btn-block' />
         </form>
