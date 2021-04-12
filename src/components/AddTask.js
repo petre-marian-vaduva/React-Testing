509fd1035acca1185a01f11f4c3e86fd7163043b
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-04-12 01:14:06 +0100

    add input and button and style them

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -7,7 +7,17 @@ const AddTask = () => {
                 <label>Task</label>
                 <input type='text' placeholder='Add task' />
             </div>
+            <div className='form-control'>
+                <label>Day & Time</label>
+                <input type='text' placeholder='Add day & time' />
+            </div>
+            <div className='form-control form-control-check'>
+                <label>Set Reminder</label>
+                <input type='checkbox' />
+            </div>
+            <input type='submit' value='Save Task' className='btn btn-block' />
         </form>
+        
     )
 }
 
