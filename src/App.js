eb5029599d8b4849cb3a100645354a234ec1f167
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-04-12 01:01:01 +0100

    add border

--- src/App.js
+++ src/App.js
@@ -32,10 +32,23 @@ const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
 }
 
+//Toggle reminder
+const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id 
+    ? { ...task, reminder: !task.reminder } 
+    : task))
+}
+
+
   return (
-    <div className="container">
+    <div className='container'>
      <Header />
-     <Tasks tasks={tasks} onDelete={deleteTask} />
+     {tasks.length > 0 ? (
+     <Tasks tasks={tasks} onDelete=
+     {deleteTask} onToggle={toggleReminder} /> 
+      ) : (
+         'No Tasks to show'
+         )}
     </div>
   )
 }
